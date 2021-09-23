e7ad8db2f50abcae3974209cbf4e9e0d45256dfa
Kaio Turubia <kaio@stymeapp.com>
2021-09-22 23:48:58 -0300

    adding support to RTSP cams

--- camera.py
+++ camera.py
@@ -4,10 +4,22 @@ import uuid
 
 # http://manpages.ubuntu.com/manpages/bionic/man1/fswebcam.1.html
 
+
 class UsbCam():
     def __init__(self):
         pass
 
     def take(self):
         os.system("fswebcam -c configs/fswebcam.conf")
+        time.sleep(2)
+
+
+class RtspCam():
+    def __init__(self, ip_cam):
+        self.ip_cam = ip_cam
+        pass
+
+    def take(self):
+        os.system(
+            "ffmpeg -y -i {} -f image2 -vframes 1 -pix_fmt yuvj420p -strftime 1 'images/%Y%m%d-%H%M%S.jpeg' -loglevel error -stats ".format(self.ip_cam))
         time.sleep(2)
--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-sudo apt-get -qy install fswebcam
+# fswebcam
+apt-get update
+apt-get -qy install fswebcam
+
+# ffmpeg
+apt-get -qy install software-properties-common
+add apt-repository ppa:mc3man/trusty-media
+apt-get update
+apt-get -qy install ffmpeg 
